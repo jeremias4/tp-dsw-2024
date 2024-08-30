86f7f59b0c5305b7a9637e7da5c6d28d9dbf1064
Josias <friasjosias1@gmail.com>
2024-08-30 07:42:38 -0300

    nuevo diseño de las pantallas de Perfil, Home y Producto

--- frontend/src/app/auth/auth.routes.ts
+++ frontend/src/app/auth/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 
 
 export const AUTH_ROUTES: Routes = [
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'editprofile', component: EditarPerfilComponent},
     {path: 'profile', component: PerfilComponent},
+    {path: 'editProfile', component: EditarPerfilComponent},
 ];
+
